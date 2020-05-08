128c92b55c2f8803358e975df1eb3a8ad72c5e78
aditya2499 <47565671+aditya2499@users.noreply.github.com>
2020-05-08 19:54:17 +0530

    Updates

---
+++ precache-manifest.ae87e5e5a3182b22c97a95b30d97207b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15ee2149d4e51e40c8a4a9a983621ef2",
+    "url": "/91social_frontend/index.html"
+  },
+  {
+    "revision": "68e8b07503dcfe36f714",
+    "url": "/91social_frontend/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "0a5f21dcff6859095f23",
+    "url": "/91social_frontend/static/css/main.2209700f.chunk.css"
+  },
+  {
+    "revision": "68e8b07503dcfe36f714",
+    "url": "/91social_frontend/static/js/2.77bd6f84.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/91social_frontend/static/js/2.77bd6f84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a5f21dcff6859095f23",
+    "url": "/91social_frontend/static/js/main.3bb17502.chunk.js"
+  },
+  {
+    "revision": "1aa9d085c1be5f2fbd22",
+    "url": "/91social_frontend/static/js/runtime-main.cea5b40f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/91social_frontend/static/media/logo.5d5d9eef.svg"
+  }
+]);
